534e4562d79c3f11c0eb5eb5ec942bd3c66a04f3
浪岡大貴 <namioka@pixel-hearts.co.jp>
2022-02-02 20:47:51 +0900

    fix 整形

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
 
   validates :name, presence: true
+  validates :self_introduction, length: { maximum: 500 }
+
   enum gender: { man: 0, woman: 1 }
 end
