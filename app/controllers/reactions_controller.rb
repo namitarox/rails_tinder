4a4d322684d36305c8b33df17120859b01214106
浪岡大貴 <namioka@pixel-hearts.co.jp>
2022-02-18 14:20:52 +0900

    fix rails6.1以降ではupdateを推奨

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -5,6 +5,6 @@ class ReactionsController < ApplicationController
         to_user_id: params[:user_id],
         from_user_id: current_user.id
       )
-    reaction.update_attributes(status: params[:reaction])
+    reaction.update(status: params[:reaction])
   end
 end
