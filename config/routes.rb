b2ab5886f28c23f80e5e055efc31408de55b49c2
浪岡大貴 <namioka@pixel-hearts.co.jp>
2022-02-18 15:34:24 +0900

    update チャットルームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   root "top#index"
   resources :users, only: %i[show index]
   resources :reactions, only: [:create]
+  resources :matching, only: [:index]
+  resources :chat_rooms, only: %i[create show]
 end
